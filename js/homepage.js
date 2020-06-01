fd167b647f230e7de8e357427361acd395998a32
gabyhartono <43226642+gabyhartono@users.noreply.github.com>
2020-06-01 14:11:07 +1000

    added back btn in project pages

--- js/homepage.js
+++ js/homepage.js
@@ -1,18 +1,4 @@
 
-// var thecircles = [];
-// function clickEffect(e){
-//   var d=document.createElement("div");
-//   var dotwidth = 50;
-//   var dotheight = 50;
-//   d.className="clickEffect";
-//   d.style.top=(e.clientY-dotheight/2)+"px";
-//   d.style.left=(e.clientX-dotwidth/2)+"px";
-//   d.style.width=dotwidth+"px";
-//   d.style.height=dotheight+"px";
-//   document.getElementById("circles").appendChild(d);
-//   thecircles.push([(e.clientY-dotheight/2), (e.clientX-dotwidth/2)]);
-// }
-// document.addEventListener('click',clickEffect);
 var Canvas = document.getElementById('circlesbg');
 var ctx = Canvas.getContext('2d');
 
@@ -47,7 +33,8 @@ function draw(){
     var cY = circlestore[i][1];
     ctx.beginPath();
     ctx.arc(cX, cY, R, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "rgb(3, 198, 252)";
+    //ctx.fillStyle = "rgb(3, 198, 252)";
+    ctx.fillStyle = circlestore[i][2];
     ctx.fill();
   };
 }
@@ -55,9 +42,10 @@ function draw(){
 function circleonclick(e){
   ctx.beginPath();
   ctx.arc(e.clientX, e.clientY, R, 0, 2 * Math.PI, false);
-  ctx.fillStyle = "rgb(3, 198, 252)";
+  randomfill = "#" + Math.floor(Math.random()*16777215).toString(16);
+  ctx.fillStyle = randomfill;
   ctx.fill();
-  circlestore.push([e.clientX, e.clientY]);
+  circlestore.push([e.clientX, e.clientY, randomfill]);
   //add coordinates of new circle for connecting line
   lines.push([e.clientX, e.clientY]);
 };
@@ -74,7 +62,7 @@ function linemove(e){
     ctx.stroke();
     ctx.beginPath();
     ctx.arc(circlestore[circlestore.length-1][0], circlestore[circlestore.length-1][1], R, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "rgb(3, 198, 252)";
+    ctx.fillStyle = circlestore[circlestore.length-1][2];
     ctx.fill();
   }
 }
